4e5bea628fd08fa96d78a6e7a0d1e1415f22fa43
eric <eric910326@gmail.com>
2024-04-01 22:52:11 +0800

    新增templates

--- mblog/urls.py
+++ mblog/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from mysite.views import homepage
+from mysite.views import homepage, showpost
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage),
+    path('post/<slug:slug>/', showpost),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from datetime import datetime
 from mysite.models import Post
 
 def homepage(request) :
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts) :
-        post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
-        post_lists.append("<small>" + str(post.body) + "</small><br><br>")
-        
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+def showpost(request, slug) :
+    try :
+        post = Post.objects.get(slug = slug)
+        if post != None :
+            return render(request, 'post.html', locals())
+    except :
+        return redirect('/')
